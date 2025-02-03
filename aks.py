c355bf390d01ba0a847f8b201b4f81c923f3d505
Miyu <72044529+miyucomics@users.noreply.github.com>
2025-02-03 13:55:20 -0500

    Optimized AKS
    Going faster is about doing less

--- aks.py
+++ aks.py
@@ -1,10 +1,8 @@
 from math import factorial
 
 def aks(n):
-    for k in range(n):
+    for k in range(1, n):
         power = factorial(n) // (factorial(k) * factorial(n - k))
-        if n - k == n:
-            power -= 1
         if power % n != 0:
             return "composite"
     return "prime"
